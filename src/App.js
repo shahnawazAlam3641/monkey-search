1eaed44df2761c9853f6f09cfc738aa08c017965
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-03-04 21:12:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-yellow-400'>hahaha ......!</div>
+    </>
+  )
+}
+
+export default App
