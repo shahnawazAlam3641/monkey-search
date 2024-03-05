b945c835bb9b25d1b0e8d2ff7f7556eddafb170d
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-03-05 17:35:55 +0530

    test apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { marvelUrl, pokeUrl, dragonBAllUrl } from './constants';
+
 
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [marvel, setMarvel] = useState(null)
+  const [pokemon, setPokemon] = useState(null)
+  const [dragonBall, setDragonBall] = useState(null)
+
+
+
+  async function fetchHeroList(){
+    const response = await fetch(marvelUrl)
+    const data = await response.json();
+    console.log(data)
+    setMarvel(data)
+  }
+
+  async function fetchPokemonList(){
+    const response = await fetch(pokeUrl)
+    const data = await response.json();
+    console.log(data)
+    setPokemon(data)
+  }
+
+  async function fetchDragonBallList(){
+    const response = await fetch(dragonBAllUrl)
+    const data = await response.json();
+    console.log(data)
+    setDragonBall(data)
+  }
+
+  useEffect(()=>{
+    fetchHeroList()
+    fetchPokemonList()
+    fetchDragonBallList()
+  },[])
+
 
   return (
     <>
-      <div className='bg-yellow-400'>hahaha ......!</div>
+
+   
+      <div className='bg-yellow-400'>{JSON.stringify(marvel).substring(0,1000)}</div>
+      <div className='bg-green-400'>{JSON.stringify(pokemon).substring(0,1000)}</div>
+      <div className='bg-gray-400'>{JSON.stringify(dragonBall).substring(0,1000)}</div>
+
     </>
   )
 }
---
+++ src/constants.jsx
@@ -0,0 +1,14 @@
+export const marvelUrl = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=53ed62ce3e964bfa8f60a5019fd69ea3&hash=253b8e47021dbca1b1188b901f04daed"
+export const marvelCharacterUrl = "http://gateway.marvel.com/v1/public/characters/1017100?ts=1&apikey=53ed62ce3e964bfa8f60a5019fd69ea3&hash=253b8e47021dbca1b1188b901f04daed"
+export const marvelPublicKey = "53ed62ce3e964bfa8f60a5019fd69ea3"
+export const marvelPrivateKey = "cc1d73777164bc264de4797380050ad24d06f82c"
+export const marvelHash = "253b8e47021dbca1b1188b901f04daed"
+
+
+export const pokeUrl = "https://pokeapi.co/api/v2/pokemon"
+
+
+export const dragonBAllUrl = "https://dragonball-api.com/api/characters"
+
+
+
